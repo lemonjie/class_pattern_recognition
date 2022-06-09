977c1cd8c64ebbf0f4b8487f81d87243daac53ef
lemonjie <wallowing0321@gmail.com>
2022-06-09 21:24:47 +0800

    kohonen done

--- kohonen_network/kohonen_network.py
+++ kohonen_network/kohonen_network.py
@@ -1,5 +1,6 @@
 import numpy as np
 import random
+import matplotlib.pyplot as plt
 
 class Data:
     def __init__(self):
@@ -37,21 +38,31 @@ class Data:
         self.train_label = label_shuf[:size]
         self.test_data = data_shuf[-size:]
         self.test_label = label_shuf[-size:]
+
+    def cal_error_rate( self, predict_label, truth_label ):
+        error_num = 0
+        for i in range(len(predict_label)):
+            if( predict_label[i]!=truth_label[i] ):
+                error_num +=1
+        print( 'error rate : ', error_num/len(predict_label) )
     
-    def draw( self, draw_name, data, label ): ###############
-        pass
+    def draw( self, draw_name, data, label ):
+        data0 = np.array([ data[i] for i in range(len(label)) if label[i]==0])
+        data1 = np.array([ data[i] for i in range(len(label)) if label[i]==1])
+
+        plt.plot(data1.T[0], data1.T[1],'og')
+        plt.plot(data0.T[0], data0.T[1],'oc')
+        plt.savefig(draw_name)
 
 
 class Kohonen:
     ### a kohonen network with only two layers, one is the 50 dims input, the other is the 2 dims output ###
-    def __init__(self, input_shape, output_dims, learn_rate=0.5, max_iter_data_times=100):
+    def __init__(self, input_shape, output_dims, learn_rate=0.5, max_iter_data_times=10):
         input_num, input_dims = input_shape
 
         self.input_dims = input_dims
         self.output_dims = output_dims
         self.weights = np.random.sample(( input_dims, output_dims ))
-        weights = self.weights / np.linalg.norm(self.weights)
-        self.weights = weights
 
         self.learn_rate = learn_rate
         self.max_iteration = input_num * max_iter_data_times
@@ -59,17 +70,16 @@ class Kohonen:
     def get_winner( self, input_x ):
         neuron = np.zeros(self.output_dims)
         for ni in range( neuron.shape[0] ):
-            for xi in range( input_x.shape[0] ):
-                neuron[ni] += self.weights[xi,ni] * input_x[xi]
+            neuron[ni] = np.vdot(self.weights[:,ni], input_x)
         winner = np.argmax(neuron)
         return winner
 
-    def update_weight( self, winner, input_x, attempts_thres=1e-5 ):
+    def update_weight( self, winner, input_x, attempts_thres=0.01 ):
         step = self.learn_rate*( input_x - self.weights[:,winner] )
         if ( np.linalg.norm(step) < attempts_thres ):
             return False
         else:
-            print(np.linalg.norm(step))
+            #print(np.linalg.norm(step))
             new_weights = self.weights[:,winner] + step
             new_weights_norm = new_weights / np.linalg.norm(new_weights)
             self.weights[:,winner] = new_weights_norm
@@ -77,20 +87,22 @@ class Kohonen:
 
     def fit( self, data ):
         for i in range( self.max_iteration ):
-            print(i, end=' ')
+            #print(i, end=' ')
             input_x = data[i%data.shape[0]] / np.linalg.norm(data[i%data.shape[0]])
             winner = self.get_winner( input_x )
             if_update = self.update_weight( winner, input_x )
-            #self.learn_rate -= i/self.max_iteration
+            self.learn_rate -= i/self.max_iteration
             if(not if_update):
                 print('No significant change so stop fitting\nAt iteration ', i, '/', self.max_iteration, sep='')
-                break
+                return
         print('End of all iteration.')
 
-    def predict( self, data ): ###########
-        pass
-
-
+    def predict( self, data ):
+        predict_label = np.empty( data.shape[0], dtype=np.int32 )
+        for i in range( data.shape[0] ):
+            input_x = data[i]
+            predict_label[i] = self.get_winner( input_x )
+        return predict_label
 
 data = Data()
 data.get_dataset( 'data0.npy', 'data1.npy' )
@@ -100,9 +112,9 @@ output_dims = len(set(data.train_label.flatten()))
 kohonen = Kohonen( input_shape, output_dims)
 
 kohonen.fit( data.train_data )
-print(kohonen.weights)
 predict_label = kohonen.predict( data.test_data )
-'''
+
+data.cal_error_rate( predict_label, data.test_label )
+
 data.draw( 'test_groundtruth.png', data.test_data, data.test_label )
 data.draw( 'test_predict.png', data.test_data, predict_label )
-'''
